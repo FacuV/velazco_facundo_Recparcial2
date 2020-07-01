b47a65406426937e640f540f39684bb6ba2b4ad6
Facundo Velazco <facundovelazco13@gmail.com>
2020-07-01 18:46:17 -0300

    prueba

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import arboles.Arbol;
+import died.estructuras.grafos.Arista;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/arboles/Arbol.java
@@ -0,0 +1,44 @@
+package arboles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Arbol<E extends Comparable<E>> {
+
+
+    protected E valor;
+
+    public abstract List<E> preOrden();
+
+    public abstract List<E> inOrden();
+
+    public abstract List<E> posOrden();
+
+    public abstract boolean esVacio();
+
+    public abstract E valor();
+
+    public abstract Arbol<E> izquierdo();
+
+    public abstract Arbol<E> derecho();
+
+    public abstract boolean contiene(E unValor);
+
+    public abstract boolean equals(Arbol<E> unArbol);
+
+    public abstract void agregar(E a);
+
+    public abstract Integer altura();
+
+    public abstract Integer profundidad();
+
+    public abstract Integer cuentaHojas();
+
+    public abstract Integer cuentaNodosDeNivel(int nivel);
+
+    public abstract boolean esCompleto();
+
+    public abstract boolean esLleno();
+
+    public abstract Arbol<E> espejar();
+}
